0a079c5d2f52eab1bad239c313781c55d516cd59
sell488 <sell488sftoday@gmail.com>
2018-04-12 12:49:53 -0500

    Testing

--- src/com/sell488/Banking/BankActions/Account.java
+++ src/com/sell488/Banking/BankActions/Account.java
@@ -6,33 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Account {
-
-    //default account
-    public void Account(){
-        accounts.put(1234, 0);
-    }
-
-    //HashMap for storing accounts and balances
-    HashMap<Integer, Integer> accounts = new HashMap<Integer, Integer>();
-
-    //Gets account balance at a specific key
-    public Integer getAccount(Integer account) {
-        return accounts.get(account);
-    }
-
-    //Gets the whole HashMap
-    public HashMap getAccountList(){
-        return accounts;
-    }
-
-    //gets accounts
-    public HashMap<Integer, Integer> getAccounts() {
-        return accounts;
-    }
-
-    //Sets the balance of an account
-    public void setAccounts(int account, int amount) {
-        accounts.put(account, amount);
-    }
+    
 
 }
